3806f981a9013eefe5fb9c5160673cb5ba425e4c
Damilola Faseun <damilola.faseun@gmail.com~>
2021-06-26 12:54:36 +0100

    uibuild

---
+++ build/precache-manifest.ca3ca0ddbff9a987342ea138d41a0115.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4468691fb4ed24de3ab974a025133df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44709403c20dea577efa",
+    "url": "/static/css/2.65c24854.chunk.css"
+  },
+  {
+    "revision": "ecb76fa9b060dc4725aa",
+    "url": "/static/css/main.3fcf59ff.chunk.css"
+  },
+  {
+    "revision": "44709403c20dea577efa",
+    "url": "/static/js/2.70355659.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.70355659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb76fa9b060dc4725aa",
+    "url": "/static/js/main.dd2668ec.chunk.js"
+  },
+  {
+    "revision": "c3bb9a24bcbfb0f1cbde",
+    "url": "/static/js/runtime-main.2cb9d682.js"
+  },
+  {
+    "revision": "f334515efd0efd42a483c572097720fc",
+    "url": "/static/media/CureImage.f334515e.png"
+  },
+  {
+    "revision": "e9067c33bdfa69530c57690fb55c6242",
+    "url": "/static/media/Hero.e9067c33.png"
+  },
+  {
+    "revision": "1d9c7945c7bc7dd0909105119bfbc191",
+    "url": "/static/media/OpenSans-Bold-webfont.1d9c7945.eot"
+  },
+  {
+    "revision": "2e90d5152ce92858b62ba053c7b9d2cb",
+    "url": "/static/media/OpenSans-Bold-webfont.2e90d515.woff"
+  },
+  {
+    "revision": "76cc6be5d8a231dc012fef4bdb86f79c",
+    "url": "/static/media/OpenSans-Bold-webfont.76cc6be5.ttf"
+  },
+  {
+    "revision": "93349923b5274a36ac93cb3168d09123",
+    "url": "/static/media/OpenSans-Bold-webfont.93349923.svg"
+  },
+  {
+    "revision": "2b4eeeaef53b3496a5cdf82803666ed7",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.2b4eeeae.svg"
+  },
+  {
+    "revision": "6218c213bb8cf22b25710da6f3a90e48",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.6218c213.eot"
+  },
+  {
+    "revision": "7657144ec477cd61ac4a5d1af3fa2d28",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.7657144e.woff"
+  },
+  {
+    "revision": "b6690626036a7d6824632769305b1978",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.b6690626.ttf"
+  },
+  {
+    "revision": "12be067a6270759b4f861d64cc267166",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.12be067a.ttf"
+  },
+  {
+    "revision": "56d9d42e23863ce0ee8e8c2e2f22843f",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.56d9d42e.eot"
+  },
+  {
+    "revision": "6ad396399f4022ccd1616cf11d0985be",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.6ad39639.woff"
+  },
+  {
+    "revision": "ea3c4697897bd7f1505facb7084c6ac3",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.ea3c4697.svg"
+  },
+  {
+    "revision": "042468300dab6f308592e15e02549c79",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.04246830.woff"
+  },
+  {
+    "revision": "5517d73acdc17143c21b0cffab4cfaa9",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.5517d73a.ttf"
+  },
+  {
+    "revision": "6512fca9ff4dc029307018c17e4a2a9d",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.6512fca9.svg"
+  },
+  {
+    "revision": "ff3bedaecdec71c34b0ca85c3c495c2d",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.ff3bedae.eot"
+  },
+  {
+    "revision": "43d5342998f3607bd61a8239e98b1160",
+    "url": "/static/media/OpenSans-Italic-webfont.43d53429.eot"
+  },
+  {
+    "revision": "5b774c25787e0a52c013463c9e3c4219",
+    "url": "/static/media/OpenSans-Italic-webfont.5b774c25.svg"
+  },
+  {
+    "revision": "de7ef31e6295902347c5c3643b2d82da",
+    "url": "/static/media/OpenSans-Italic-webfont.de7ef31e.ttf"
+  },
+  {
+    "revision": "f42641eed834f7b97a9499362c6c8855",
+    "url": "/static/media/OpenSans-Italic-webfont.f42641ee.woff"
+  },
+  {
+    "revision": "09e00aa7622ece30a0f1e06b55f66c2a",
+    "url": "/static/media/OpenSans-Light-webfont.09e00aa7.eot"
+  },
+  {
+    "revision": "2e98fc3ce85f31f63010b706259cb604",
+    "url": "/static/media/OpenSans-Light-webfont.2e98fc3c.ttf"
+  },
+  {
+    "revision": "45b47f3e9c7d74b80f5c6e0a3c513b23",
+    "url": "/static/media/OpenSans-Light-webfont.45b47f3e.woff"
+  },
+  {
+    "revision": "8f04ed9aeb2185499068d84842b95aa1",
+    "url": "/static/media/OpenSans-Light-webfont.8f04ed9a.svg"
+  },
+  {
+    "revision": "1d22953c479914c2f801e08de666b0e8",
+    "url": "/static/media/OpenSans-LightItalic-webfont.1d22953c.ttf"
+  },
+  {
+    "revision": "550b5fda4a27cfedb7131b1a6e85e748",
+    "url": "/static/media/OpenSans-LightItalic-webfont.550b5fda.eot"
+  },
+  {
+    "revision": "b553da506077488bc65289e10841d527",
+    "url": "/static/media/OpenSans-LightItalic-webfont.b553da50.woff"
+  },
+  {
+    "revision": "fd6dd5fa10c5a74f0a767eeb695342f1",
+    "url": "/static/media/OpenSans-LightItalic-webfont.fd6dd5fa.svg"
+  },
+  {
+    "revision": "488d5cc145299ba07b75495100419ee6",
+    "url": "/static/media/OpenSans-Regular-webfont.488d5cc1.ttf"
+  },
+  {
+    "revision": "79515ad0788973c533405f7012dfeccd",
+    "url": "/static/media/OpenSans-Regular-webfont.79515ad0.woff"
+  },
+  {
+    "revision": "8185eb3059c46e4169ce107dfcf85950",
+    "url": "/static/media/OpenSans-Regular-webfont.8185eb30.svg"
+  },
+  {
+    "revision": "c4d82460ef260eb1589e73528cbfb257",
+    "url": "/static/media/OpenSans-Regular-webfont.c4d82460.eot"
+  },
+  {
+    "revision": "3f6b1eed8a0832d6f316fc26526348a8",
+    "url": "/static/media/OpenSans-Semibold-webfont.3f6b1eed.svg"
+  },
+  {
+    "revision": "697574b47bcfdd2c45e3e63c7380dd67",
+    "url": "/static/media/OpenSans-Semibold-webfont.697574b4.woff"
+  },
+  {
+    "revision": "b32acea6fd3c228b5059042c7ad21c55",
+    "url": "/static/media/OpenSans-Semibold-webfont.b32acea6.ttf"
+  },
+  {
+    "revision": "f28eb362fb6afe946d822ee5451c2146",
+    "url": "/static/media/OpenSans-Semibold-webfont.f28eb362.eot"
+  },
+  {
+    "revision": "64f886b232962979e2eaf29d93108286",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.64f886b2.ttf"
+  },
+  {
+    "revision": "70bafcaaadad9e17b9c7784abbc6b1c2",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.70bafcaa.eot"
+  },
+  {
+    "revision": "70eb93d7ba2ad241180085a9a74b0b95",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.70eb93d7.svg"
+  },
+  {
+    "revision": "719f7321a8366f4ee609737026432113",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.719f7321.woff"
+  },
+  {
+    "revision": "9e9ad9ec19a7835aa34273e6990f62b1",
+    "url": "/static/media/asd.9e9ad9ec.png"
+  },
+  {
+    "revision": "af9e943a96b7c3bb07d3b56fb164f085",
+    "url": "/static/media/asd.af9e943a.jpg"
+  },
+  {
+    "revision": "045ccda8679b7cb8be7d505ea1f6eaed",
+    "url": "/static/media/asthma.045ccda8.jpg"
+  },
+  {
+    "revision": "64e71853406ebed129a2ed62e972d69d",
+    "url": "/static/media/asthma.64e71853.png"
+  },
+  {
+    "revision": "71e1999e359acdf1ab58a954687fc1d0",
+    "url": "/static/media/diabetics.71e1999e.png"
+  },
+  {
+    "revision": "ddd03d31e79402f895bd24764fb2d15d",
+    "url": "/static/media/diabetics.ddd03d31.jpg"
+  },
+  {
+    "revision": "b5877fd874abdd4b06d65421dad8d914",
+    "url": "/static/media/facebook.b5877fd8.svg"
+  },
+  {
+    "revision": "126dca25a9f167d9b251ae68ea4f502f",
+    "url": "/static/media/instagram.126dca25.svg"
+  },
+  {
+    "revision": "1f33342ee7e4d0ab5bca8b60419be807",
+    "url": "/static/media/lowSpermCount.1f33342e.jpg"
+  },
+  {
+    "revision": "69174a82f916c247d5cc8eaef58d08ae",
+    "url": "/static/media/lowSpermCount.69174a82.png"
+  },
+  {
+    "revision": "2ffcea2e8be450d767372f21758c5b8a",
+    "url": "/static/media/nav-logo.2ffcea2e.png"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  },
+  {
+    "revision": "bc00eb070263b03317d9673ed82037a5",
+    "url": "/static/media/parkinsons.bc00eb07.jpg"
+  },
+  {
+    "revision": "d535968efc0153b6e4f44dc85d2d4d42",
+    "url": "/static/media/parkinsons.d535968e.png"
+  },
+  {
+    "revision": "f6b49963265aac8dd3fb2b24efbc4de5",
+    "url": "/static/media/sickle-cell.f6b49963.png"
+  },
+  {
+    "revision": "a1b6270a632ae8827a0985be81e3e713",
+    "url": "/static/media/sickleCell1.a1b6270a.jpg"
+  },
+  {
+    "revision": "1bedb66c88162de9c2eb9773c384ac09",
+    "url": "/static/media/twitter-squared.1bedb66c.svg"
+  }
+]);
